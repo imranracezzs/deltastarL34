efe485a3dd3a7179bbc0a023225755e0a5474f59
imranracezzs <2305a21l34@sru.edu.in>
2024-11-22 12:34:42 +0530

    Update deltastar.py

--- deltastar.py
+++ deltastar.py
@@ -22,8 +22,8 @@ R31 = st.number_input("Enter R31 (Resistance between 3 and 1)", min_value=0.0, s
 if st.button("Calculate STAR Resistances"):
     if R12 > 0 and R23 > 0 and R31 > 0:
         R1, R2, R3 = DELTA_STAR(R12, R23, R31)
-        st.write(f"R1 (Star) = {R1:.2f} Ohms")
-        st.write(f"R2 (Star) = {R2:.2f} Ohms")
-        st.write(f"R3 (Star) = {R3:.2f} Ohms")
+        st.write(f"R1 (Star) = {R1:.2f} kOhms")
+        st.write(f"R2 (Star) = {R2:.2f} kOhms")
+        st.write(f"R3 (Star) = {R3:.2f} kOhms")
     else:
         st.error("Please enter valid positive resistance values.")
